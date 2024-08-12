f200826043619520d2a4540f58228b44c87dbf89
den021912 <usakovdenis440@gmail.com>
2024-08-12 15:30:43 +0400

    Update dz16_4.py

--- dz16_4.py
+++ dz16_4.py
@@ -12,45 +12,45 @@ class User(BaseModel):
     id: int = None
 
 @app.get('/')
-def get_all_messages() -> List[User]:
+async def get_all_user() -> List[User]:
     return users
 
 @app.get(path = "/user/{user_id}")
-def get_message(user_id: int) -> User:
+async def get_user(user_id: int) -> User:
     try:
         return users[user_id]
     except IndexError:
         raise HTTPException(status_code=404, detail='User was not found')
 
-@app.post('/user')
-def create_message(user: User) -> str:
-    user.id = len(users)
+@app.post('/user/{username}/{age}')
+async def create_user(user: User, username: str, age: int) -> User:
+    user_id = max(users, key=lambda x: int(x.id)).id + 1 if users else 1
+    user = User(id=user_id, username=username, age=age)
     users.append(user)
-    return f'message created'
+    return user
+    
 
 @app.put("/user/{user_id}/{username}/{age}")
-def update_message(user_id: int, user: str = Body()) -> str:
-    try:
-        edit_message = users[user_id]
-        edit_message.text = user
-        return f'Message updated'
-    except IndexError:
-        raise HTTPException(status_code=404, detail='User was not found')
+async def update_user(user_id: int, username: str, age: int) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail='User was not found')
+
 
 @app.delete("/user/{user_id}")
-def delete_message(user_id: int) -> str:
+async def delete_user(user_id: int) -> str:
     try:
         users.pop(user_id)
-        return f"Message ID = {user_id} deleted"
+        return f"User ID = {user_id} deleted"
     except IndexError:
         raise HTTPException(status_code=404, detail='User was not found')
 
 @app.delete("/")
-def kill_messages_all() -> str:
+async def kill_user_all() -> str:
     users.clear()
     return 'All Messages deleted'
-
-
-
 
 
